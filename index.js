6cfa8eb40bb451715e0c70115c8b119734178609
Luisa Schrott <luisa.schrott@gmail.com>
2019-06-28 12:43:22 +0200

    done

--- index.js
+++ index.js
@@ -1,12 +1,61 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe"); // Import of the model Recipe from './models/Recipe'
+const data = require("./data.js"); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose
+  .connect("mongodb://localhost/recipeApp", { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
+Recipe.create({
+  title: "Spagetti",
+
+  level: "Easy Peasy",
+
+  ingredients: ["Spagetti", "Tomatos", "Onions"],
+
+  cuisine: "Itian",
+
+  dishType: "Dish",
+
+  duration: 20,
+  creator: "Manu"
+})
+  .then(data => {
+    console.log("Recipe is created:", data.title);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+Recipe.insertMany(data).then(
+  data.forEach(element => {
+    console.log(element.title);
+  })
+);
+
+Recipe.findOneAndUpdate(
+  { title: "Rigatoni alla Genovese" },
+  { duration: 100 },
+  { new: true }
+)
+  .then(recipe => {
+    console.log("Success", recipe);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+Recipe.deleteOne({ title: "Carrot Cake" })
+  .then(recipe => {
+    console.log("Recipe is deleted", recipe);
+    mongoose.connection.close();
+  })
+  .catch(err => {
+    console.log(err);
+  });
